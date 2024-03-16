335aa73c246fd26e20075ff218854abf38025ebf
Vlad B <bondar.vladislav1121@vu.cdu.edu.ua>
2024-03-16 19:00:15 +0200

    first commit

---
+++ Assets/Move.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+    public GameObject goal;
+    Vector3 dir;
+    public float speed = 2f;
+    void Start() {
+      //  dir = goal.transform.position - this.transform.position;
+      //  this.transform.position = this.transform.position + dir;
+        //this.transform.Translate(dir);
+    }
+
+    private void LateUpdate() {
+        dir = goal.transform.position - this.transform.position;
+        this.transform.LookAt(goal.transform.position);
+        if (dir.magnitude > 2)
+        {
+            Vector3 velocity = dir.normalized * speed * Time.deltaTime;
+            this.transform.position = this.transform.position + velocity;
+        }
+    }
+}
